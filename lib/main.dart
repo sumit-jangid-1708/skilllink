a030ee70d3bbd236ef34b29709326041809c300b
sumit-jangid-1708 <140996958+sumit-jangid-1708@users.noreply.github.com>
2026-05-07 23:35:38 +0530

    change title of the app

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'SkillLink',
       translations: Languages(),
       locale: const Locale('hi', 'IN'),
       fallbackLocale: const Locale('en', 'US'),
